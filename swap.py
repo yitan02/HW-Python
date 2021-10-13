11f72bbc52289b49b6985ec4138f5aba3fb98e41
Yinglin Tan <yingtan114@gmail.com>
2021-10-12 21:46:40 -0700

    added missing list input and fixed the inability to swap items

--- swap.py
+++ swap.py
@@ -1,8 +1,4 @@
-def swap_last_item():
-    list = []
-    for i in range (n):
-        data = int(input())
-        list.append(data)
+def swap_last_item(list):
     temp = list[-1]
     list[-1] = list[0]
     list[0] = temp

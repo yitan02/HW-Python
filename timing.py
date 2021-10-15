c837b28bce4ba3e456eaa04b71460ff72e2091e2
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 23:43:53 -0700

    added comments

--- timing.py
+++ timing.py
@@ -1,8 +1,38 @@
 import time
 def calculate_time(func):
     def wrapper():
-        start_time = time.time()
-        func()
-        end_time = time.time()
-        print(f'Total time {end_time-start_time}')
+    """
+    Compute the time it takes for the function to run.
+  
+    This function takes the current time as the starting time. Then, the 
+    function runs and end time is calculated by calling time.time() again.
+    Prints the total time taken to run by subtracting end time with start 
+    time.
+    
+    Parameter
+    _________
+    func : function
+        A function that contains code that will be called on.
+    
+    Returns
+    _______
+    int
+       The time taken where end time subtracts start time
+    
+    Example
+    _______
+    >>> calculate_time(func):
+    # start_time = 00:00:01
+    # end_time = 00:00:02
+    2
+    >>> calculator_time(func):
+    # start_time = 00:00:10
+    # end_time = 00:00:05
+    5
+    """
+
+       start_time = time.time()
+       func()
+       end_time = time.time()
+       print(f'Total time {end_time-start_time}')
     return wrapper 

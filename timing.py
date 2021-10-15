f3c47aaaff18787997a3e6a7946ec9de7480159b
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 22:07:57 -0700

    testing print statement

--- timing.py
+++ timing.py
@@ -5,5 +5,5 @@ def calculate_time(func):
         func()
         end_time = time.time()
         time_taken = end_time-start_time
-        print(time_taken)
+        print("Hello. This is after function executes.")
     return wrapper 

3de2eb33865193c0200cc664206d3e86d2b0054a
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 21:37:27 -0700

    added print statements to show input and output lists

--- swap.py
+++ swap.py
@@ -1,6 +1,10 @@
 
 def swap_last_item(list):
+    input = list 
     temp = list[-1]
     list[-1] = list[0]
     list[0] = temp
+    output = list
+    print(f'Input: {input}')
+    print(f'Output: {output}')
     return list

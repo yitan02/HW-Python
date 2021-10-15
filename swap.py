93bc6b984a721d77435c6b0ff00e9e7fae1396b8
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 22:48:26 -0700

    revert back to previous changes

--- swap.py
+++ swap.py
@@ -1,9 +1,9 @@
 
 def swap_last_item(list):
     input = list 
-   # temp = list[-1]
-   # list[-1] = list[0]
-   # list[0] = temp
+    temp = list[-1]
+    list[-1] = list[0]
+    list[0] = temp
     output = []
     output.append(list[-1])
     output.extend(list[1:-1])

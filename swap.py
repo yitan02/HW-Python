efea669d6ef0003447d1e6a40fb2eeb6f23e39ff
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 22:43:40 -0700

    testing output print

--- swap.py
+++ swap.py
@@ -4,9 +4,10 @@ def swap_last_item(list):
     temp = list[-1]
     list[-1] = list[0]
     list[0] = temp
+    print(list[0])
     output = []
     output.append(list[0])
-    output.extend(list[1:-2])
+    output.extend(list[1:-1])
     output.append(list[-1])
     print(f'Input: {input}')
     print(f'Output: {output}')

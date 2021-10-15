223d6a8e5a5889109fb03151180247121b27c29f
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 20:13:23 -0700

    added multiplcation

--- calculator.py
+++ calculator.py
@@ -18,6 +18,8 @@ def calculator(number1,number2,operator):
              return number1//number2
        elif operator == '**' :
              return number1**number2
+       elif operator == '*'
+             return number1*number2
 
 def parse_input(user_input):
     number1 = user_input.split()[0]

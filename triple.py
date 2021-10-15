c483ad252694b4d6b1a8cb653ae1cb6b3e422cd2
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 23:48:32 -0700

    added comments

--- triple.py
+++ triple.py
@@ -1,5 +1,33 @@
 def tripler(func):
     def wrapper():
+    """
+    Calls a function 3 times.
+      
+    This function has nothing much special and calls a function three times.
+    
+    Parameter
+    _________
+    func : function
+        A function with code that the decorator calls.
+    
+    Returns
+    _______
+    function
+        Outputs results of a function
+    
+    Example
+    _______
+    >>> tripler(func)
+    # say the function adds 2+2
+    4
+    4
+    4
+    >>> tripler(func)
+    # say the function prints "hello"
+    hello
+    hello
+    hello
+    """
         func()
         func()
         func()

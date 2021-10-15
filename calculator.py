f57703454b492edd3151e50c02d391fe78f3b9a5
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 19:04:29 -0700

    fixed syntax with if statements

--- calculator.py
+++ calculator.py
@@ -4,15 +4,15 @@ def calculator(number1,number2,operator):
           message = input("Enter equation: ")
           if message and allowed.issuperset(message):
              return False 
-          if operator == '+'
+          if operator == '+' :
              return number1 + number2
-          else if operator == '-'
+          elif operator == '-' :
              return number1 - number2
-          else if operator == '/'
+          elif operator == '/' :
              return number1/number2
-          else if operator == '//'
+          elif operator == '//' :
              return number1//number2
-          else if operator == '**'
+          elif operator == '**' :
              return number1**number2
 
 def parse_input(user_input):

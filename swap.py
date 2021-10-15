eebb289287f18a901fbf7178f4dc75ae426f3eb2
Yinglin Tan <yingtan114@gmail.com>
2021-10-14 22:46:11 -0700

    swap in output rather than by variables

--- swap.py
+++ swap.py
@@ -1,14 +1,13 @@
 
 def swap_last_item(list):
     input = list 
-    temp = list[-1]
-    list[-1] = list[0]
-    list[0] = temp
-    print(list[0])
+   # temp = list[-1]
+   # list[-1] = list[0]
+   # list[0] = temp
     output = []
-    output.append(list[0])
-    output.extend(list[1:-1])
     output.append(list[-1])
+    output.extend(list[1:-1])
+    output.append(list[0])
     print(f'Input: {input}')
     print(f'Output: {output}')
     return list
